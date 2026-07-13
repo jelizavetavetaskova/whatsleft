b6676232c525676f933b6fc558eba85c6518ec5c
Jeļizaveta Vetaškova <s24vetajeli@venta.lv>
2026-07-13 20:35:05 +0300

    Initial project

---
+++ src/main/java/dev/vulpden/whatsleft/WhatsleftApplication.java
@@ -0,0 +1,13 @@
+package dev.vulpden.whatsleft;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WhatsleftApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WhatsleftApplication.class, args);
+    }
+
+}
---
+++ src/test/java/dev/vulpden/whatsleft/WhatsleftApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.vulpden.whatsleft;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WhatsleftApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
