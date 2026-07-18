1a77b32b84c5c9ee95b8887704e80817d2a25601
Jeļizaveta Vetaškova <s24vetajeli@venta.lv>
2026-07-18 22:29:45 +0300

    feat: added a controller for getting a recipe by id

--- src/main/java/dev/vulpden/whatsleft/recipe/controller/RecipeController.java
+++ src/main/java/dev/vulpden/whatsleft/recipe/controller/RecipeController.java
@@ -5,9 +5,14 @@ import dev.vulpden.whatsleft.recipe.service.IRecipeCRUDService;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PageableDefault;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.NoSuchElementException;
 
 @RestController
 @RequestMapping("/api/recipes")
@@ -19,7 +24,18 @@ public class RecipeController {
     }
 
     @GetMapping
-    public Page<RecipeListDto> getRecipes(@PageableDefault(size = 20) Pageable pageable) {
+    public Page<RecipeListDto> getAll(@PageableDefault(size = 20) Pageable pageable) {
         return recipeService.getAllRecipes(pageable);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Object> getById(@PathVariable Long id) {
+        try {
+            return ResponseEntity.ok(recipeService.getRecipeById(id));
+        } catch (IllegalArgumentException e) {
+            return ResponseEntity.badRequest().body(e.getMessage());
+        } catch (NoSuchElementException e) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(e.getMessage());
+        }
     }
 }
