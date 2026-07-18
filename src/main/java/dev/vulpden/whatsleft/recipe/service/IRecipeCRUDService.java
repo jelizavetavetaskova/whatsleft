fba203d621dfc1f626df0a3155a1e4cd08478967
Jeļizaveta Vetaškova <s24vetajeli@venta.lv>
2026-07-18 22:18:33 +0300

    feat: added a service for getting a recipe by id

--- src/main/java/dev/vulpden/whatsleft/recipe/service/IRecipeCRUDService.java
+++ src/main/java/dev/vulpden/whatsleft/recipe/service/IRecipeCRUDService.java
@@ -1,9 +1,12 @@
 package dev.vulpden.whatsleft.recipe.service;
 
+import dev.vulpden.whatsleft.recipe.dto.RecipeDetailDto;
 import dev.vulpden.whatsleft.recipe.dto.RecipeListDto;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 
 public interface IRecipeCRUDService {
     Page<RecipeListDto> getAllRecipes(Pageable pageable);
+
+    RecipeDetailDto getRecipeById(Long recipeId);
 }
--- src/main/java/dev/vulpden/whatsleft/recipe/service/impl/RecipeCRUDService.java
+++ src/main/java/dev/vulpden/whatsleft/recipe/service/impl/RecipeCRUDService.java
@@ -1,12 +1,16 @@
 package dev.vulpden.whatsleft.recipe.service.impl;
 
 import dev.vulpden.whatsleft.recipe.RecipeMapper;
+import dev.vulpden.whatsleft.recipe.dto.RecipeDetailDto;
 import dev.vulpden.whatsleft.recipe.dto.RecipeListDto;
+import dev.vulpden.whatsleft.recipe.model.Recipe;
 import dev.vulpden.whatsleft.recipe.repo.IRecipeRepo;
 import dev.vulpden.whatsleft.recipe.service.IRecipeCRUDService;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
+
+import java.util.NoSuchElementException;
 
 @Service
 public class RecipeCRUDService implements IRecipeCRUDService {
@@ -19,5 +23,16 @@ public class RecipeCRUDService implements IRecipeCRUDService {
     @Override
     public Page<RecipeListDto> getAllRecipes(Pageable pageable) {
         return recipeRepo.findAll(pageable).map(RecipeMapper::toListDto);
+    }
+
+    @Override
+    public RecipeDetailDto getRecipeById(Long recipeId) {
+        if (recipeId == null) throw new IllegalArgumentException("Recipe id must not be null");
+        if (recipeId <= 0) throw new IllegalArgumentException("Recipe id must be greater than 0");
+
+        Recipe recipe = recipeRepo.findById(recipeId)
+                .orElseThrow(() -> new NoSuchElementException("Recipe with this id does not exist"));
+
+        return RecipeMapper.toDetailDto(recipe);
     }
 }
